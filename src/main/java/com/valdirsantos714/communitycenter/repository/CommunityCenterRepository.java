be8c93488c9f52a8fac8d17e4c169560c95cd296
Valdir Santos <valdirsantost40@gmail.com>
2024-09-03 15:25:16 -0300

    feat: created CommunityCenterRepository

---
+++ src/main/java/com/valdirsantos714/communitycenter/repository/CommunityCenterRepository.java
@@ -0,0 +1,12 @@
+package com.valdirsantos714.communitycenter.repository;
+
+import com.valdirsantos714.communitycenter.model.CommunityCenter;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommunityCenterRepository extends MongoRepository<CommunityCenter, Long> {
+    List<CommunityCenter> findByCurrentOccupancyGreaterThan(int occupancy);
+}
